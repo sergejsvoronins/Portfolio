99a36f14b79257e1ae643567160480bfe6a1020e
sergejsvoronins <svoronins@gmail.com>
2022-11-01 22:22:40 +0100

    has connected 2 pages. Started with project part design

---
+++ src/JS/modules/projectrepo.js
@@ -0,0 +1,10 @@
+export class ProjectRepo{
+    constructor(name, url, description, technique, author, date){
+        this.name = name;
+        this.url = url;
+        this.description = description;
+        this.technique = technique;
+        this.author = author;
+        this.date = date;
+    }
+}
